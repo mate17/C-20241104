6316413e7b77c42cdf4e1e41977dce2f5e8351b5
weisz.mate <weisz.mate@28-DIAK14>
2024-11-04 15:32:35 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using C_20241104;
+using System.Text;
+
+List<Versenyzo> versenyzok = [];
+
+using StreamReader sr = new("..\\..\\..\\src\\forras.txt", Encoding.UTF8);
+while (!sr.EndOfStream)
+{
+    versenyzok.Add(new(sr.ReadLine()));
+}
+
+Console.WriteLine($"Versenyzők száma: {versenyzok.Count()}");
+
+//csopi 3
+
+//var f1 = versenyzok.Count(v => v.Kategoria == "25-29");
+//Console.WriteLine($"Versenyzők száma: {f1}");
+
+//var f2 = versenyzok.Average(v => 2024 - v.Szulev);
+//Console.WriteLine($"átlag: {f2}");
+
+//var f3 = versenyzok.Sum(v => v.Idok["uszas"].TotalHours);
+//Console.WriteLine($"teljes úszás idő: {Math.Round(f3,2)}");
+
+//var f4 = versenyzok.Where(v => v.Kategoria == "elit").Average(v => v.Idok["uszas"].TotalMinutes);
+//Console.WriteLine($"f4: {f4:0.00}");
+
+//var f5 = versenyzok.Where(v => v.Nem).MinBy(v => v.Osszido);
+//Console.WriteLine($"f5: {f5}");
+
+//var f6 = versenyzok.GroupBy(v => v.Kategoria).OrderBy(g => g.Key);
+//foreach (var grp in f6)
+//{
+//    Console.WriteLine($"{grp.Key}: {grp.Count(),2}");
+//}
+
+//var f7 = versenyzok.GroupBy(v => v.Kategoria).ToDictionary(g => g.Key, g => g.Average(v => v.Idok["I depo"].TotalMinutes + v.Idok["II depo"].TotalMinutes)).OrderBy(kvp => kvp.Key);
+//Console.WriteLine("Átlag depó idő kategoriánként");
+//foreach (var kvp in f7)
+//{
+//    Console.WriteLine($"\t{kvp.Key}: {kvp.Value:0.00} perc");
+//}
+
+//csopi 1
+
+//var f1 = versenyzok.Count(v => v.Kategoria == "elit");
+//Console.WriteLine($"Versenyzők száma: {f1}");
+
+//var f2 = versenyzok.Where(v => v.Nem == false).Average(v => 2024 - v.Szulev);
+//Console.WriteLine($"átlag: {f2}");
+
+//var f3 = versenyzok.Sum(v => v.Idok["kerekparozas"].TotalHours);
+//Console.WriteLine($"teljes úszás idő: {Math.Round(f3,2)}");
+
+//var f4 = versenyzok.Where(v => v.Kategoria == "elit junior").Average(v => v.Idok["uszas"].TotalMinutes);
+//Console.WriteLine($"f4: {f4:0.00}");
+
+//var f5 = versenyzok.Where(v => v.Nem == true).MinBy(v => v.Osszido);
+//Console.WriteLine($"f5: {f5}");
+
+//var f6 = versenyzok.GroupBy(v => v.Kategoria).OrderBy(g => g.Key);
+//foreach (var grp in f6)
+//{
+//    Console.WriteLine($"{grp.Key}: {grp.Count(),2}");
+//}
+
+//csopi 2
+
+//var f1 = versenyzok.Count(v => v.Kategoria == "elit junior");
+//Console.WriteLine($"Versenyzők száma: {f1}");
+
+//var f2 = versenyzok.Where(v => v.Nem == true).Average(v => 2024 - v.Szulev);
+//Console.WriteLine($"átlag: {f2}");
+
+//var f3 = versenyzok.Sum(v => v.Osszido/60/60);
+//Console.WriteLine($"teljes úszás idő: {f3:0.00}");
+
+//var f4 = versenyzok.Where(v => v.Kategoria == "20-24").Average(v => v.Idok["uszas"].TotalMinutes);
+//Console.WriteLine($"f4: {f4:0.00}");
+
+//var f5 = versenyzok.Where(v => v.Nem == false).MinBy(v => v.Osszido);
+//Console.WriteLine($"f5: {f5}");
+
+//var f6 = versenyzok.GroupBy(v => v.Nem).OrderBy(g => g.Key);
+//foreach (var grp in f6)
+//{
+//    if (grp.Key == true)
+//    {
+//        Console.WriteLine($"Férfi: {grp.Count(),2}");
+//    }
+//    else if (grp.Key == false)
+//    {
+//        Console.WriteLine($"Nő: {grp.Count(),2}");
+//    }
+//}
---
+++ Versenyzo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_20241104
+{
+    internal class Versenyzo
+    {
+        public string Nev { get; set; }
+        public int Szulev { get; set; }
+        public int Rajtszam { get; set; }
+        public bool Nem { get; set; }
+        public string Kategoria { get; set; }
+        public Dictionary<string, TimeSpan> Idok { get; set; }
+
+        public override string ToString()
+        {
+            return $"[{Rajtszam}] {Nev} ({Kategoria})";
+        }
+
+        public int Osszido => (int)Idok.Values.Sum(x => x.TotalSeconds);
+
+        public Versenyzo(string sor)
+        {
+            string[] s = sor.Split(";");
+            Nev = s[0];
+            Szulev = int.Parse(s[1]);
+            Rajtszam = int.Parse(s[2]);
+            if (s[3] == "f")
+            {
+                Nem = true;
+            }
+            else
+            {
+                Nem = false;
+            }
+            Kategoria = s[4];
+            Idok = new()
+            {
+                {"uszas", TimeSpan.Parse(s[5])},
+                {"I depo", TimeSpan.Parse(s[6])},
+                {"kerekparozas", TimeSpan.Parse(s[7])},
+                {"II depo", TimeSpan.Parse(s[8])},
+                {"futas", TimeSpan.Parse(s[9])}
+            };
+        }
+    }
+}
